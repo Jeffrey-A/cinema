becf6f217742cd345af2f29a5dcf852adc72a1df
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2024-04-18 08:34:43 -0400

    feat(SeatsController): implement purchase endpoint

--- src/main/java/com/example/cinema/SeatsController.java
+++ src/main/java/com/example/cinema/SeatsController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 
 @RestController
 public class SeatsController {
+    List<Seat> purchasedSeats = new ArrayList<>();
 
     @GetMapping("/seats")
      public SeatData getSeatsData() {
@@ -23,7 +26,8 @@ public class SeatsController {
         List<Seat> seats = new ArrayList<>();
         for (int row = 1; row <= seatData.getRows(); row++) {
             for (int column = 1; column <= seatData.getColumns(); column++) {
-                seats.add(new Seat(row, column));
+                int price = row <= 4 ? 10 : 8; 
+                seats.add(new Seat(row, column, price));
             }
         }
         seatData.setSeats(seats);
@@ -31,13 +35,36 @@ public class SeatsController {
     }
 
     @PostMapping("/purchase")
-    public ResponseEntity<SeatWithPrice> purchaseSeat(@RequestBody Seat seat) {
+    public ResponseEntity<?> purchaseSeat(@RequestBody Seat seat) {
         int row = seat.getRow();
         int column = seat.getColumn();
 
-        SeatWithPrice purchaseSeat = new SeatWithPrice(row, column, 10);
+        if (row > 9 || column > 9) {
+            ErrorResponse errorResponse = new ErrorResponse("The number of a row or a column is out of bounds!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if (!this.isSeatAvailable(seat)) {
+            ErrorResponse errorResponse = new ErrorResponse("The ticket has been already purchased!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        Seat purchaseSeat = new Seat(row, column, 10);
+        purchasedSeats.add(purchaseSeat);
         
         return ResponseEntity.ok(purchaseSeat);
+    }
+
+    public boolean isSeatAvailable(Seat seat) {
+        int row = seat.getRow();
+        int column = seat.getColumn();
+
+        for (Seat purchasedSeat: this.purchasedSeats) {
+            if (purchasedSeat.getRow() == row && purchasedSeat.getColumn() == column) {
+                return false;
+            }
+        }
+        return true;
     }
     
     static class SeatData {
@@ -73,10 +100,12 @@ public class SeatsController {
     static class Seat {
         private int row;
         private int column;
+        private int price;
 
-        public Seat(int row, int column) {
+        public Seat(int row, int column, int price) {
             this.row = row;
             this.column = column;
+            this.price = price;
         }
 
         public int getRow() {
@@ -94,16 +123,6 @@ public class SeatsController {
         public void setColumn(int column) {
             this.column = column;
         }
-    }
-
-    static class SeatWithPrice extends Seat {
-        private int price = 0;
-
-        SeatWithPrice(int row, int column, int price) {
-            super(row, column);
-            this.price = price;
-
-        }
 
         public void setPrice (int price) {
             this.price = price;
@@ -111,6 +130,21 @@ public class SeatsController {
 
         public int getPrice() {
             return this.price;
+        }
+    }
+    static class ErrorResponse {
+        private String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
         }
     }
 
